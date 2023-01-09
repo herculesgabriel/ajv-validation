53c5311bedd6007d359346e33be1085b89b2eb1e
Hercules Gabriel <herculesgabriel00@gmail.com>
2023-01-09 20:35:23 -0300

    feat: add route to create a character

--- index.mjs
+++ index.mjs
@@ -1,16 +1,40 @@
 import express from 'express'
 
 import { mongoConnection } from './database.mjs'
+import { characterValidation } from './character-validation.mjs'
 
 const app = express()
 
-app.get('/characters', async (_request, response) => {
-  const collection = mongoConnection.collection('characters')
+app.use(express.json())
 
-  const characters = await collection.find().toArray()
+app
+  .route('/characters')
+  .get(async (_request, response) => {
+    const collection = mongoConnection.collection('characters')
 
-  response.status(200).json({ characters })
-})
+    const characters = await collection.find().toArray()
+
+    response.status(200).json({ characters })
+  })
+  .post(async (request, response) => {
+    const { real_name, nickname, description } = request.body
+    const characterData = { real_name, nickname, description }
+
+    const valid = characterValidation(characterData)
+
+    if (!valid) {
+      return response.status(400).json({ error: characterValidation.errors })
+    }
+
+    const collection = mongoConnection.collection('characters')
+    const { insertedId, acknowledged } = await collection.insertOne(characterData)
+
+    if (!acknowledged) {
+      return response.status(500).json({ error: 'Internal server error' })
+    }
+
+    response.status(201).json({ character: insertedId })
+  })
 
 app.listen(3000, () => {
   console.info('[server] Running at port 3000')
